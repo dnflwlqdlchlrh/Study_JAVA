953d2c559dda588a4497a365edcb679411fdec21
허윤 <huryoun5025@gmail.com>
2023-03-15 15:36:03 +0900

    문자열, 문자 배열_Array

--- J_pro03/src/exam01/Sample03.java
+++ J_pro03/src/exam01/Sample03.java
@@ -4,16 +4,22 @@ package exam01;
 public class Sample03 {
 
 	public static void main(String[] args) {
-		String[] arr1 = new String[5];
-		
-		for(int i = 0; i < arr1.length; i++) {
-			arr1[i] = "Hello";
-		}
+		String[] arr1 = new String[] {"A", "B", "C", "D", "E"};
 		
 		for(int i = 0; i < arr1.length; i++) {
 			System.out.printf("arr1[%d] -> %s\n", i, arr1[i]);
 		}
-
+		
+		System.out.println("-------------");
+		
+		char[] arr2 = new char[5] ;
+		
+		for(int i = 0; i < arr1.length; i++) {
+			arr2[i] = (char)('a' + i);
+		}
+		
+		for(int i = 0; i < arr2.length; i++) {
+			System.out.printf("arr2[%d] -> %s\n", i, arr2[i]);
+		}
 	}
-
 }
