bfaf908475ce506816533bbf6854894cf6fa657f
허윤 <huryoun5025@gmail.com>
2023-03-16 16:12:46 +0900

    생활코딩_JAVA1

--- JAVA1/src/AccountingClassApp.java
+++ JAVA1/src/AccountingClassApp.java
@@ -1,20 +1,10 @@
-
-public class AccountingClassApp {
+class Accounting{
 	
-	public static double valueofSupply;
-	public static double vatRate;
-	public static double expenseRate;
+	public double valueofSupply;
+	public double vatRate;
+	public double expenseRate;
 	
-	public static void main(String[] args) {
-		
-		valueofSupply = 20000.0;
-		vatRate = 0.1;
-		expenseRate = 0.3;
-		print();
-		
-	}
-
-	public static void print() {
+	public void print() {
 		System.out.println("Value of supply : " + valueofSupply);
 		System.out.println("VAT : " + getVAT());
 		System.out.println("Total : " + getTotal());
@@ -25,31 +15,64 @@ public class AccountingClassApp {
 		System.out.println("Dividend3 : " + getDividend3());
 	}
 
-	public static double getDividend3() {
+	public double getDividend3() {
 		return getIncome() * 0.2;
 	}
 
-	public static double getDividend2() {
+	public double getDividend2() {
 		return getIncome() * 0.3;
 	}
 
-	public static double getDividend1() {
+	public double getDividend1() {
 		return getIncome() * 0.5;
 	}
 
-	public static double getIncome() {
+	public double getIncome() {
 		return valueofSupply - getExpense();
 	}
 
-	public static double getExpense() {
+	public double getExpense() {
 		return valueofSupply * expenseRate;
 	}
 
-	public static double getTotal() {
+	public double getTotal() {
 		return valueofSupply + getVAT();
 	}
 
-	public static double getVAT() {
+	public double getVAT() {
 		return valueofSupply * vatRate;
+	}
+}
+
+
+
+
+public class AccountingClassApp {
+	
+	
+	public static void main(String[] args) {
+		
+//		Accounting2.valueofSupply = 10000.0;
+//		Accounting2.vatRate = 0.1;
+//		Accounting2.expenseRate = 0.3;
+//		Accounting2.print();
+//		
+//		anotherVariable = ...;
+//		anotherMethod = ...;
+//		
+//		Accounting1.valueofSupply = 20000.0;
+//		Accounting1.vatRate = 0.05;
+//		Accounting1.expenseRate = 0.2;
+//		Accounting1.print();
+		
+//		Instance
+		Accounting a1 = new Accounting();
+		a1.valueofSupply = 10000.0;
+		a1.vatRate = 0.1;
+		a1.expenseRate = 0.3;
+		a1.print();
+		
+		
+		
 	}
 }
