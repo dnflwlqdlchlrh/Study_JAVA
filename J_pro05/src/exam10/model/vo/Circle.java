08232f2c3c128c3bdcd46c784b9c0de64b4550e4
허윤 <huryoun5025@gmail.com>
2023-03-22 17:49:02 +0900

    Class 작성 문제 및 풀이

--- J_pro05/src/exam10/model/vo/Circle.java
+++ J_pro05/src/exam10/model/vo/Circle.java
@@ -6,14 +6,17 @@ public class Circle {
 	private int radius = 1;
 	
 	public void incrementRadius() {
+		// 원 반지름의 크기를 +1 시키는 함수
 		radius++;
 	}
 	
 	public void getAreaOfCircle() {
+		// 원 너비를 출력하는 함수
 		System.out.println(PI * Math.pow(radius, 2));
 	}
 	
 	public void getSizeOfCircle() {
+		// 원 둘레를 출력하는 함수
 		System.out.println(PI * radius * 2);
 	}
 
