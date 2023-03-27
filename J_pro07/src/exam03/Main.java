1372ccdbd015def655cdef136e9728861c203215
허윤 <huryoun5025@gmail.com>
2023-03-27 16:19:02 +0900

    상속 예제

---
+++ J_pro07/src/exam03/Main.java
@@ -0,0 +1,18 @@
+package exam03;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Subject s1 = new Subject("국어");
+		Subject s2 = new Subject("영어", 78.3);
+		
+		System.out.println(s1.getName());
+		System.out.println(s2.getName());
+		
+		System.out.println(s2.getGrade());
+		System.out.println(s2.getGrade().getPoint());
+		
+	}
+
+}
