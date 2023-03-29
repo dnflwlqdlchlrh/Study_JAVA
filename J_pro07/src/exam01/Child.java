12568869695debf06d5e649cbbb9ded554459783
허윤 <huryoun5025@gmail.com>
2023-03-29 09:31:03 +0900

    상속(Inheritance)

--- J_pro07/src/exam01/Child.java
+++ J_pro07/src/exam01/Child.java
@@ -7,15 +7,16 @@ public class Child extends Parent{
 //	}
 	
 	public void printNumber() {
-		setNumber(3);
+		setNumber(3);	// 자식 setNumber Method에 매개 변수 값 입력
+		System.out.println("오버라이딩 결과 값 : " + getNumber());	// 9번
 	}
 	
 	
 	@Override
 	public void setNumber(int number) {
-		System.out.println("자식의 setNumber Method 동작 시작");
-		super.setNumber(number);
-		System.out.println("자식의 setNumber Method 동작 끝");
+		System.out.println("자식의 setNumber Method 동작 시작");	// 1번
+		super.setNumber(number);	// 2번, 입력 된 값으로 부모의 setNumber로 호출
+		System.out.println("자식의 setNumber Method 동작 끝"); // 8번
 	}
 
 }
--- J_pro07/src/exam01/Parent.java
+++ J_pro07/src/exam01/Parent.java
@@ -17,9 +17,11 @@ public class Parent {
 	}
 	
 	public void setNumber(int number) {
-		System.out.println("부모의 setNumber Method 동작 시작");
-		this.number = number;
-		System.out.println("부모의 setNumber Method 동작 끝");
+		System.out.println("오버라이딩 이전 부모 Class의 멤버 변수 값 : " + this.number);	// 3번
+		System.out.println("부모의 setNumber Method 동작 시작");	// 4번
+		this.number = number;	// 5번
+		System.out.println("오버라이딩 이후 부모 Class의 멤버 변수 값 : " + this.number);	// 6번
+		System.out.println("부모의 setNumber Method 동작 끝"); // 7번
 	}
 	
 	
