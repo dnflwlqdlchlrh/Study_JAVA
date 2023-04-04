e4ffbbdcf5006538a6434623b06ead8e7031903d
허윤 <huryoun5025@gmail.com>
2023-04-04 14:17:34 +0900

    Wrapper Class 적용

--- J_pro08/src/exam01/Sample02.java
+++ J_pro08/src/exam01/Sample02.java
@@ -1,72 +1,90 @@
 package exam01;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Sample02 {
 	
-	public String str;
+	Scanner sc = new Scanner(System.in);
 	
-	public void ex01(String str) {
+	public void ex01() {
 		/*
-		 * 사용자 입력으로 정수값 입력을 받을 때 한 번의 입력으로 1개 이상의 정수값을
-		 * 받을 수 있도록 한다.(nextLine() Method를 사용해야 한다.)
+		 * 사용자 입력으로 정수값 입력을 받을 때 한번의 입력으로 1개 이상의 정수값을
+		 * 받을 수 있도록 한다.(nextLine() 메서드를 사용해야 함)
+		 * 
+		 * 예)
+		 *     정수값 입력 : 10 20 30 40 50
+		 *     
+		 *     다음의 값을 입력하였습니다.
+		 *     10, 20, 30, 40, 50
 		 */
-		this.str = str;
-		int num = Integer.parseInt(this.str);
-		System.out.println(num);
+		System.out.print("정수값 입력 : ");
+		String input = sc.nextLine();
+		String[] sArr = input.split(" ");
 		
+		int[] iArr = new int[sArr.length];
+		for(int i = 0; i < sArr.length; i++) {
+			iArr[i] = Integer.parseInt(sArr[i]);
+		}
+		System.out.println(Arrays.toString(iArr));
+		
+		System.out.println("다음의 값을 입력하였습니다.");
+		System.out.println(String.join(", ", sArr));
 	}
 	
-	public void ex02(String str) {
+	public void ex02() {
 		/*
 		 * 사용자 입력으로 전화번호를 입력 받을 때 xxx-xxxx-xxxx 형식으로 입력하지 않은 경우
 		 * 다시 입력하도록 한다.
 		 */
-		this.str = str;
-		boolean isPhone = this.str.matches("\\d{3}-\\d{4}-\\d{4}");
-	
-		if(isPhone == true) {
-			System.out.println(this.str);
-		} else {
-			System.out.println("다시 입력해주세요.");
+		String input = "";
+		while(!input.matches("\\d{3}-\\d{4}-\\d{4}")) {
+			System.out.print("전화번호 입력 : ");
+			input = sc.nextLine();
+			
+			/*
+			 *  정규표현식을 사용하지 않는 경우
+			 *      1. "-" 를 구분자로 하는 문자열 배열 크기가 3개 만들어 지는지 확인
+			 *      2. 전화번호 형식에 맞추어 각 배열에 저장된 문자열 길이가 3, 4, 4 인지 확인
+			 *      3. 숫자로만 구성된 문자열인지 확인.
+			 */
 		}
-		
+		System.out.println("전화번호 입력을 확인하였습니다.");
 	}
 	
-	public void ex03(String str) {
+	public void ex03() {
 		/*
 		 * 사용자 입력으로 전화번호를 입력 받고 출력할 때 전화번호 마지막 4자리 숫자는
 		 * 다음의 문자로 마스킹 처리하여 출력되도록 한다. (마스킹 문자 -> *)
 		 */
-		this.str = str;
-		String[] sArr1 = this.str.split("-");
-		for(int i = 0; i < 4; i++) {
-			sArr1[i + 7] = "*";
-		}
-		String sArr2 = String.join("-", sArr1);
-		System.out.println(sArr2);
+		System.out.print("전화번호 입력 : ");
+		String input = sc.nextLine();
 		
+		// String masking = input.replace(input.substring(input.length() - 4, input.length()), "****");
+		// String masking = input.substring(0, input.length() - 4) + "****";
+		String[] sArr = input.split("-");
+		sArr[2] = "****";
+		String masking = String.join("-", sArr);
+		System.out.println(masking);
 	}
 	
-	public void ex04(String str) {
+	public void ex04() {
 		/*
-		 * 사용자 입력으로 이메일 주소 형식을 받을 때 xxxxx@gmail.com 또는 xxxxx@naver.com,
-		 * xxxxx@nate.com의 주소 형식만 받을 수 있도록 한다. (아이디 글자 수 제한 X)
+		 * 사용자 입력으로 이메일 주소 형식을 입력 받을 때 xxxxx@gmail.com 또는 xxxxx@naver.com,
+		 * xxxxx@nate.com 의 주소 형식만 받을 수 있도록 한다.(xxxxx 는 5글자 제한 아님)
 		 */
-		
-		this.str = str;
-		
 		String[] emailDomain = new String[] {
 				"gmail.com", "naver.com", "nate.com"
 		};
 		
 		while(true) {
+			System.out.print("이메일 주소 입력 : ");
+			String input = sc.nextLine();
+			
 			// @가 포함된 이메일 주소 형식인지 확인
-			String[] email = this.str.split("@");
+			String[] email = input.split("@");
 			if(email.length == 2) {
-				boolean isValiad =false;
+				boolean isValiad = false;
 				// 지정된 도메인 주소가 맞는지 확인
 				for(int i = 0; i < emailDomain.length; i++) {
 					if(email[1].equals(emailDomain[i])) {
@@ -79,32 +97,16 @@ public class Sample02 {
 					break;
 				} else {
 					System.out.println("허용된 이메일 주소 도메인이 아닙니다.");
-					break;
 				}
 			} else {
 				System.out.println("이메일 주소 형식이 아닙니다. 다시 입력하세요.");
-				break;
 			}
 		}
-		
-		
 	}
 
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		Sample02 exam01 = new Sample02();
-		exam01.ex01(br.readLine());
-		
-		Sample02 exam02 = new Sample02();
-		exam02.ex02(br.readLine());
-		
-		Sample02 exam04 = new Sample02();
-		exam04.ex04(br.readLine());
-		
-		
-
+	public static void main(String[] args) {
+		Sample02 sample = new Sample02();
+		sample.ex01();
 	}
 
 }
