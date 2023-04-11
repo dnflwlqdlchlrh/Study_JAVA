d40a82e94fa124ff3eeef6c532244ed34221f5b8
허윤 <huryoun5025@gmail.com>
2023-04-11 17:09:40 +0900

    패스워드 초기화 기능 구현

--- J_pro09/src/model/vo/Account.java
+++ J_pro09/src/model/vo/Account.java
@@ -1,5 +1,7 @@
 package model.vo;
 
+import java.util.Random;
+
 public abstract class Account {
 	private String name;
 	private String password;
@@ -31,5 +33,29 @@ public abstract class Account {
 		// 학생용 계정은 STD_ 접두사가 붙어서 생성되게 한다.
 		// 선생님용 계정은 TCH_ 접두사가 붙어서 생성되게 한다.
 	}
-	public abstract String resetPassword();
+	public String resetPassword() {
+		Random rand = new Random();
+		String newPass = "";
+		
+		int cnt = 0;
+		while(true) {
+			boolean tf = rand.nextBoolean();
+			if(tf == true) {
+				newPass += (char)(rand.nextInt(26) + 65);
+				cnt++;
+				if(cnt == 6) {
+					break;
+				}
+			}
+			if(tf == false) {
+				newPass += (char)(rand.nextInt(26) + 97);
+				cnt++;
+				if(cnt == 6) {
+					break;
+				}
+			}
+		}
+		setPassword(newPass);
+		return newPass;
+	}
 }
--- J_pro09/src/model/vo/Teacher.java
+++ J_pro09/src/model/vo/Teacher.java
@@ -35,27 +35,8 @@ public class Teacher extends Account{
 	@Override
 	public String resetPassword() {
 		String prefix = "TCH_";
-		Random rand = new Random();
-		String newPass = "";
-		
-		int cnt = 0;
-		while(true) {
-			boolean tf = rand.nextBoolean();
-			if(tf == true) {
-				newPass += (char)(rand.nextInt(26) + 65);
-				cnt++;
-				if(cnt == 6) {
-					break;
-				}
-			}
-			if(tf == false) {
-				newPass += (char)(rand.nextInt(26) + 97);
-				cnt++;
-				if(cnt == 6) {
-					break;
-				}
-			}
-		}
-		return prefix + newPass;
+		String newPass = super.resetPassword();
+		setPassword(prefix + newPass); 
+		return newPass;
 	}
 }
