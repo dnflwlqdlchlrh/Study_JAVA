fd0383862e701361bf8a827d89c4d48be81c054c
허윤 <huryoun5025@gmail.com>
2023-04-13 14:13:53 +0900

    Exception, 예외 처리

---
+++ J_Pro10/src/exam01/Sample01.java
@@ -0,0 +1,21 @@
+package exam01;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyException m = new MyException();
+		
+		System.out.println("에러 발생 전");
+		try {
+			m.exceptionThrows();
+		} catch(Exception e) {
+//			System.out.println("Sample01.main 10번 줄 Exception 발생!");
+			e.printStackTrace();
+		}
+//		m.exceptionNonThrows();
+		System.out.println("에러 발생 후 처리 완료!");
+
+	}
+
+}
