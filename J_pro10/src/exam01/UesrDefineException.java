c54f232906bcd5b2fd8944bfae94a3bdab863f86
허윤 <huryoun5025@gmail.com>
2023-04-13 15:22:37 +0900

    Exception, 예외 처리

---
+++ J_pro10/src/exam01/UesrDefineException.java
@@ -0,0 +1,10 @@
+package exam01;
+
+
+public class UesrDefineException extends Exception {
+	
+	public UesrDefineException() {}
+	public UesrDefineException(String message) {
+		super(message);
+	}
+}
